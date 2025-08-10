4e2280a3306777dd7d2da575a5d0ecc267194c4f
S4N <dcruzhe@unal.edu.co>
2025-08-10 04:11:36 -0500

    Feat: Set rules for API Validations

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,11 @@ import { configSwagger } from './docs/Swagger.docs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: false
+  }));
   configSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
